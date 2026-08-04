4f319acf3727e97f2a5f54be5a452503983df09a
durgaakhil0211 <durgaakhil0211@gmail.com>
2026-08-04 15:11:25 +0530

    added  main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "main" {
   location            = azurerm_resource_group.main.location
 
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
   # --- Security guardrails ---
   min_tls_version                   = "TLS1_2"
