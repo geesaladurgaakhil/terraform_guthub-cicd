5164721ebef62c04b4493ef2bed88c7e52334b0b
durgaakhil0211 <durgaakhil0211@gmail.com>
2026-08-06 15:03:04 +0530

    added filesdd

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_storage_account" "this" {
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  min_tls_version                  = "TLS1_2"
-  allow_nested_items_to_be_public  = false
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
 
   tags = merge(var.tags, {
     environment = var.environment
